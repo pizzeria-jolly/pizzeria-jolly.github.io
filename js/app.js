ee36cea149867dcbc0b3cd792a2043d6a8df3a23
pizzeria-jolly <stefanobicciato@gmail.com>
2021-05-25 01:02:28 +0200

    listino pagina 1

--- js/app.js
+++ js/app.js
@@ -40,8 +40,224 @@ function contattiClick() {
   click.to(boh,1,{marginTop:'60px',opacity:0})
         .to(boh,1,{marginTop:'0',opacity:1});
   setTimeout(function () {
-    boh.innerHTML = `<div class="done" style="font-size:100px;">yesss</div>`
-  },1000);
+    boh.innerHTML = `<div class="pagina">
+
+      <div class="conteinerListino1">
+        <div class="pizza">
+          <div class="nome">Marinara</div>
+          <div class="ingredienti">Pomodoro, aglio, origano</div>
+          <div class="prezzo">4.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Margherita</div>
+          <div class="ingredienti">Pomodoro, mozzarella</div>
+          <div class="prezzo">5.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Pugliese</div>
+          <div class="ingredienti">Pom, moz, cipolla</div>
+          <div class="prezzo">4.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Romana</div>
+          <div class="ingredienti">Pom, mozz, acciughe</div>
+          <div class="prezzo">5.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Prosciutto</div>
+          <div class="ingredienti">Pom, mozz, prosciutto</div>
+          <div class="prezzo">5.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Funghi</div>
+          <div class="ingredienti">Pom, mozz, funghi</div>
+          <div class="prezzo">5.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Diavola</div>
+          <div class="ingredienti">Pom, mozz, salamino piccante</div>
+          <div class="prezzo">6.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Viennese</div>
+          <div class="ingredienti">Pom, mozz, wurstel</div>
+          <div class="prezzo">5.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Prosciutto e Funghi</div>
+          <div class="ingredienti">Pom, mozz, prosciutto, funghi</div>
+          <div class="prezzo">6.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Capricciosa</div>
+          <div class="ingredienti">Pom, mozz, prosciutto, funghi, carciofi</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">4 Stagioni</div>
+          <div class="ingredienti">Pom, mozz, prosciutto, funghi, carciofi, olive</div>
+          <div class="prezzo">7.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">4 Formaggi</div>
+          <div class="ingredienti">Pom, mozz, formaggi misti (gorgonzola a richiesta)</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Tonno e Cipolla</div>
+          <div class="ingredienti">Pom, mozz, tonno, cipolla</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Patatosa</div>
+          <div class="ingredienti">Pom, mozz, patatine</div>
+          <div class="prezzo">5.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Patatine e Wurstel</div>
+          <div class="ingredienti">Pom, mozz, patatine, wurstel</div>
+          <div class="prezzo">6.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Patatine e Salsiccia</div>
+          <div class="ingredienti">Pom, mozz, patatine, salsiccia</div>
+          <div class="prezzo">6.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Patatine e Salamino</div>
+          <div class="ingredienti">Pom, mozz, patatine, salamino picc.</div>
+          <div class="prezzo">6.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Parmigiana</div>
+          <div class="ingredienti">Pom, mozz, melanzane, grana</div>
+          <div class="prezzo">6.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Spinaci e Ricotta</div>
+          <div class="ingredienti">Pom, mozz, ricotta, spinaci</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Tonno e Peperoni</div>
+          <div class="ingredienti">Pom, mozz, tonno, peperoni</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Rucola e Grana</div>
+          <div class="ingredienti">Pom, mozz, rucola, grana</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+      </div>
+      <div class="conteinerListino2">
+        <div class="pizza">
+          <div class="nome">Jolly</div>
+          <div class="ingredienti">Pom, mozz, briè, crudo, rucola, grana</div>
+          <div class="prezzo">8.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Philadelphia</div>
+          <div class="ingredienti">Pom, mozz, tris di funghi, philadelphia, grana</div>
+          <div class="prezzo">8.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Crema del Piave</div>
+          <div class="ingredienti">Pom, mozz, stracchino, chiodini, prosciutto, salamino picc.</div>
+          <div class="prezzo">8.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Tigre</div>
+          <div class="ingredienti">Pom, mozz, fontina, porcini, speck</div>
+          <div class="prezzo">7.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Boscaiola</div>
+          <div class="ingredienti">Pom, mozz, tris di funghi, grana</div>
+          <div class="prezzo">7.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Golosa</div>
+          <div class="ingredienti">Pom, mozz, pancetta, provola, radicchio</div>
+          <div class="prezzo">7.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Delizia</div>
+          <div class="ingredienti">Pom, mozz, crema del piave, radicchio, prosciutto</div>
+          <div class="prezzo">7.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Veneta</div>
+          <div class="ingredienti">Pom, mozz, melanzane, salsiccia, funghi</div>
+          <div class="prezzo">7.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Profumi di Bosco</div>
+          <div class="ingredienti">Pom, mozz, tris di funghi, noci, grana</div>
+          <div class="prezzo">8.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Montana</div>
+          <div class="ingredienti">Pom, mozz, funghi, asiago, speck</div>
+          <div class="prezzo">7.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Misto Funghi</div>
+          <div class="ingredienti">Pom, mozz, porcini, chiodini, champignon</div>
+          <div class="prezzo">7.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Carbonara</div>
+          <div class="ingredienti">Pom, mozz, pancetta, uovo, grana</div>
+          <div class="prezzo">7.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Porcini e Grana</div>
+          <div class="ingredienti">Pom, mozz, porcini, grana</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Gorgonzola e Speck</div>
+          <div class="ingredienti">Pom, mozz, gorgonzola, speck</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Bresaola e Grana</div>
+          <div class="ingredienti">Pom, mozz, bresaola, grana</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Asiago e Porcini</div>
+          <div class="ingredienti">Pom, mozz, porcini, asiago</div>
+          <div class="prezzo">7.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Genovese</div>
+          <div class="ingredienti">Pom, mozz, pesto, olive, noci</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Parma</div>
+          <div class="ingredienti">Pom, mozz, prosciutto crudo</div>
+          <div class="prezzo">6.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Piccantissima</div>
+          <div class="ingredienti">Pom, mozz, salamino picc., peperoni, peperoncino</div>
+          <div class="prezzo">6.50 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Bufalina</div>
+          <div class="ingredienti">Pom, mozzarella di bufala senza conservanti</div>
+          <div class="prezzo">6.00 €</div>
+        </div>
+        <div class="pizza">
+          <div class="nome">Cavallina</div>
+          <div class="ingredienti">Pom, mozz, sfilacci, rucola, grana</div>
+          <div class="prezzo">7.50 €</div>
+        </div>
+      </div>
+    </div>`
+  },1000); //inserire html del listino
   boh.classList.add('prova');
   console.log(boh.classList[1]);
 };
