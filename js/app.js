d789b99003dcc54904eccf595d8685cac13c92c6
pizzeria-jolly <stefanobicciato@gmail.com>
2021-05-24 17:03:08 +0200

    anim

--- js/app.js
+++ js/app.js
@@ -1,1 +1,53 @@
+const home = document.querySelector('.home');
+const listino = document.querySelector('.listino');
+const novita = document.querySelector('.novita');
+const contatti = document.querySelector('.contatti');
 
+// funzioni
+
+function mouseHoverC() {
+  const timeHover = new TimelineMax();
+
+    timeHover.fromTo(contatti,1,{opacity: 1}, {opacity: 0.5})
+    .fromTo(contatti,1,{opacity:0.5},{opacity:1});
+};
+
+function mouseHoverN() {
+  const timeHover = new TimelineMax();
+
+    timeHover.fromTo(novita,1,{opacity: 1}, {opacity: 0.5})
+    .fromTo(novita,1,{opacity:0.5},{opacity:1});
+};
+
+function mouseHoverL() {
+  const timeHover = new TimelineMax();
+
+    timeHover.fromTo(listino,1,{opacity: 1}, {opacity: 0.5})
+    .fromTo(listino,1,{opacity:0.5},{opacity:1});
+};
+
+function mouseHoverH() {
+  const timeHover = new TimelineMax();
+
+    timeHover.fromTo(home,1,{opacity: 1}, {opacity: 0.5})
+    .fromTo(home,1,{opacity:0.5},{opacity:1});
+};
+
+
+
+// fine funzioni
+
+const timel = new TimelineMax();
+
+timel.fromTo(contatti, 1, {x: '-90vw'}, {x: '0%', ease: Power2.easeInOut})
+      .fromTo(novita, 1, {x: '-90vw'}, {x: '0%', ease: Power2.easeInOut})
+      .fromTo(listino, 1, {x: '-90vw'}, {x: '0%', ease: Power2.easeInOut})
+      .fromTo(home, 1, {x: '-90vw'}, {x: '0%', ease: Power2.easeInOut})
+      .fromTo(contatti,1, {padding: '5px'},{padding: '20px'})
+      .to(contatti,1, {padding: '5px'});
+
+
+contatti.addEventListener('mouseover', mouseHoverC);
+novita.addEventListener('mouseover', mouseHoverN);
+listino.addEventListener('mouseover', mouseHoverL);
+home.addEventListener('mouseover', mouseHoverH);
