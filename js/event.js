71700bc714c6d28b843c705869eb87769032e52a
pizzeria-jolly <stefanobicciato@gmail.com>
2021-05-26 03:02:15 +0200

    gif

--- js/event.js
+++ js/event.js
@@ -1,6 +1,2 @@
 contatti.addEventListener('click', contattiClick);
-contatti.addEventListener('mouseover', mouseHoverC);
-novita.addEventListener('mouseover', mouseHoverN);
-listino.addEventListener('mouseover', mouseHoverL);
-listino.addEventListener('click', listinoClick)
-home.addEventListener('mouseover', mouseHoverH);
+listino.addEventListener('click', listinoClick);
